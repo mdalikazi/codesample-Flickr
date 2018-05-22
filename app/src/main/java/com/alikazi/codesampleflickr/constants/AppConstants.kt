88b3138a92de2079625fd44f7de6c76a19272b90
Ali Kazi <mdalikazi@gmail.com>
2018-05-22 13:21:00 +1000

    Added AppCenter crashlytics

--- app/src/main/java/com/alikazi/codesampleflickr/constants/AppConstants.kt
+++ app/src/main/java/com/alikazi/codesampleflickr/constants/AppConstants.kt
@@ -8,6 +8,8 @@ class AppConstants {
     companion object {
         const val LOG_TAG_MAIN = "Main"
         const val LOG_TAG_NETWORK = "Network"
+
+        const val APP_CENTER_SECRET = "2fca532a-3a87-475e-917f-4d05c488dcff"
     }
 
 }
